a0f891ea6e30d5099c71846c4a11b3356c0a74de
Carlo Bramini <braminicarlo@live.com>
2020-03-15 14:27:16 +0000

    Created unit test, with dependecy injection of BonusAllocationService. Downgraded the ninject package for compatability in the Test Project to 3.3.3

--- Solution/SynetecMvcAssessment.Test/UnitTest1.cs
+++ Solution/SynetecMvcAssessment.Test/UnitTest1.cs
@@ -1,14 +1,126 @@
 ﻿using System;
+using InterviewTestTemplatev2.BusinessLayer;
+using InterviewTestTemplatev2.Infrastructure;
+using InterviewTestTemplatev2.Infrastructure.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
 
 namespace SynetecMvcAssessment.Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IKernel _NinjectStandardKernel { get; set; }
+        private IBonusAllocationService _IBonusAllocationService { get; set; }
+
+        public UnitTest1()
         {
+            _NinjectStandardKernel = new StandardKernel(new NinjectDiFactory());
+            this._IBonusAllocationService = this._NinjectStandardKernel.Get<IBonusAllocationService>();
+        }
+
+        [TestMethod]
+        public void Negative_TotalSalary_Should_Throw_Exception()
+        {
+            //arrange
+            int totalSalary = -200;
+            int employeeSalary = 100;
+            int totalBonusPool = 1000;
+
+            try
+            {
+                //act
+                this._IBonusAllocationService.CalculateBonusAllocation(totalSalary, employeeSalary, totalBonusPool);
+            }
+            catch (TotalSalaryCannotBeNegativeException ex)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false);
+            }
+
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void Negative_EmployeeSalary_Should_Throw_Exception()
+        {
+            //arrange
+            int totalSalary = 200;
+            int employeeSalary = -100;
+            int totalBonusPool = 1000;
+
+            try
+            {
+                //act
+                this._IBonusAllocationService.CalculateBonusAllocation(totalSalary, employeeSalary, totalBonusPool);
+            }
+            catch (EmployeeSalaryCannotBeNegativeException ex)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false);
+            }
+
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void Negative_TotalBonusPool_Should_Throw_Exception()
+        {
+            //arrange
+            int totalSalary = 200;
+            int employeeSalary = 100;
+            int totalBonusPool = -1000;
+
+            try
+            {
+                //act
+                this._IBonusAllocationService.CalculateBonusAllocation(totalSalary, employeeSalary, totalBonusPool);
+            }
+            catch (TotalBonusPoolCannotBeNegativeException ex)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false);
+            }
+
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void EmployeeSalary_GreaterThan_TotalaSalary_Should_Throw_Exception()
+        {
+            //arrange
+            int totalSalary = 200;
+            int employeeSalary = 1001;
+            int totalBonusPool = 1000;
+
+            try
+            {
+                //act
+                this._IBonusAllocationService.CalculateBonusAllocation(totalSalary, employeeSalary, totalBonusPool);
+            }
+            catch (EmployeeSalaryIsGreaterThanTotalSalaryException ex)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false);
+            }
+
+            Assert.IsTrue(false);
         }
     }
 }
