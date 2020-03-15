dcbfaf2b032b6b59a63c720ca055f2223f8acb4b
Carlo Bramini <braminicarlo@live.com>
2020-03-15 13:46:10 +0000

    create BonusAllocationService to calculate the bonus. Added also the exception for the CalculateBonusAllocation() method

---
+++ Solution/SynetecMvcAssessment/BusinessLayer/BonusAllocationService.cs
@@ -0,0 +1,52 @@
+﻿using InterviewTestTemplatev2.Infrastructure.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewTestTemplatev2.BusinessLayer
+{
+    public interface IBonusAllocationService
+    {
+        int CalculateBonusAllocation(int totalSalary, int employeeSalary, int totalBonusPool);
+    }
+
+
+    public class BonusAllocationService : IBonusAllocationService
+    {
+        /// <summary>
+        /// totalSalary: the sum of all employees salaries
+        /// employeeSalary: the salary for a single employee
+        /// totalBonusPool: the bonul pool that needs to be devides among employees
+        /// </summary>
+        /// <param name="totalSalary"></param>
+        /// <param name="employeeSalary"></param>
+        /// <param name="totalBonusPool"></param>
+        /// <returns></returns>
+        public int CalculateBonusAllocation(int totalSalary, int employeeSalary, int totalBonusPool)
+        {
+
+            if (totalSalary < 0)
+            {
+                throw new TotalSalaryCannotBeNegativeException("ZR: totalSalary cannot be less than 0");
+            }
+            if (employeeSalary < 0)
+            {
+                throw new EmployeeSalaryCannotBeNegativeException("ZR: employeeSalary cannot be less than 0");
+            }
+            if (totalBonusPool < 0)
+            {
+                throw new TotalBonusPoolCannotBeNegativeException("ZR: totalBonusPool cannot be less than 0");
+            }
+            if (employeeSalary > totalSalary)
+            {
+                throw new EmployeeSalaryIsGreaterThanTotalSalaryException("ZR: a single employeeSalary cannot be > than all the employees totalSalary");
+            }
+
+            decimal bonusPercentage = (decimal)employeeSalary / (decimal)totalSalary;
+            int bonusAllocation = (int)(bonusPercentage * totalBonusPool);
+
+            return bonusAllocation;
+        }
+    }
+}
---
+++ Solution/SynetecMvcAssessment/Infrastructure/Exceptions/SyntecMvcException.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewTestTemplatev2.Infrastructure.Exceptions
+{
+    public class TotalSalaryCannotBeNegativeException : Exception
+    {
+        public TotalSalaryCannotBeNegativeException()
+        {
+        }
+
+        public TotalSalaryCannotBeNegativeException(string message)
+            : base(message)
+        {
+        }
+
+        public TotalSalaryCannotBeNegativeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class EmployeeSalaryCannotBeNegativeException : Exception
+    {
+        public EmployeeSalaryCannotBeNegativeException()
+        {
+        }
+
+        public EmployeeSalaryCannotBeNegativeException(string message)
+            : base(message)
+        {
+        }
+
+        public EmployeeSalaryCannotBeNegativeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class TotalBonusPoolCannotBeNegativeException : Exception
+    {
+        public TotalBonusPoolCannotBeNegativeException()
+        {
+        }
+
+        public TotalBonusPoolCannotBeNegativeException(string message)
+            : base(message)
+        {
+        }
+
+        public TotalBonusPoolCannotBeNegativeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class EmployeeSalaryIsGreaterThanTotalSalaryException : Exception
+    {
+        public EmployeeSalaryIsGreaterThanTotalSalaryException()
+        {
+        }
+
+        public EmployeeSalaryIsGreaterThanTotalSalaryException(string message)
+            : base(message)
+        {
+        }
+
+        public EmployeeSalaryIsGreaterThanTotalSalaryException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
